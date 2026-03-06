bb460d16ef781da435d931a08c633d06ef5180e6
Bilal Shahid <bilalshahid187@gmail.com>
2026-03-07 01:04:15 +0500

    depl fixes

--- server/core/database.py
+++ server/core/database.py
@@ -6,15 +6,7 @@ from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
 
-DATABASE_USER = os.getenv("DATABASE_USER", "postgres")
-DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD", "")
-DATABASE_HOST = os.getenv("DATABASE_HOST", "localhost")
-DATABASE_PORT = os.getenv("DATABASE_PORT", "5433")
-DATABASE_NAME = os.getenv("DATABASE_NAME", "casebot")
-
-pg_url = f"postgresql://postgres:Bilal%409002@localhost:5433/casebot"
-
-engine = create_engine(pg_url)
+engine = create_engine(os.getenv("DATABASE_PUBLIC_URL"))
 
 
 def get_session():
